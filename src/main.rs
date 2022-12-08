5d436351d6686f17a1922ea18d2ff88711750b97
handschrift <cahit.kilinc@gmx.de>
2022-12-08 19:15:38 +0100

    Now calling the download function in the install match arm

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 
 use mcpm::cli::{Action, Arguments, generate_application_files};
 use mcpm::minecraft::init;
-use mcpm::modrinth_wrapper::search;
+use mcpm::modrinth_wrapper::{download, search};
 
 fn main() {
     let appdata_path = generate_application_files().expect("Couldn't create the data directory or the datafiles");
@@ -12,7 +12,9 @@ fn main() {
         Action::Search { mod_name, limit } => {
             search(mod_name, limit).expect("failed to search");
         }
-        Action::Install { mod_name } => {}
+        Action::Install { mod_name } => {
+            download(mod_name);
+        }
         Action::Init { minecraft_path } => {
             init(appdata_path.as_path(), minecraft_path).expect("Failed to initialize the minecraft instance");
         }
