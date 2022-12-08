46049a9d6404868d39c0cf44e7bb6982fe66b958
handschrift <cahit.kilinc@gmx.de>
2022-12-08 19:15:56 +0100

    Adding structs to represent a mod and its corresponding versions

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,4 +1,25 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
-pub struct Mod {}
+pub struct Mod {
+    pub slug: String,
+    pub title: String,
+    pub description: String,
+    pub project_type: String,
+    pub versions: Vec<String>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct ModVersion {
+    pub id: String,
+    pub game_versions: Vec<String>,
+    pub loaders: Vec<String>,
+    pub files: Vec<ModFile>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct ModFile {
+    pub url: String,
+    pub filename: String,
+    pub size: u32,
+}
