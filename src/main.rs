9be24816d54121337b699543ceda13b7283abf1c
handschrift <cahit.kilinc@gmx.de>
2023-01-06 19:21:32 +0100

    Now reading instance data on startup

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 use clap::Parser;
 
 use mcpm::cli::{Action, Arguments, generate_application_files};
-use mcpm::minecraft::init;
+use mcpm::minecraft::{init, MinecraftData, MinecraftInstance};
 use mcpm::modrinth_wrapper::{download, search};
 
 fn main() {
-    let appdata_path = generate_application_files().expect("Couldn't create the data directory or the datafiles");
+    let appdata_path = generate_application_files()
+        .expect("Couldn't create the data directory or the datafiles");
     let args: Arguments = Arguments::parse();
+    let minecraft_data = MinecraftData::parse_existing(appdata_path.as_path())
+        .expect("Couldn't parse the current data");
+    let current_minecraft_instance = match minecraft_data.get_default_instance() {
+        Some(t) => t,
+        None => {
+            println!("No default minecraft instance found... please do a mcpm init");
+            MinecraftInstance::new()
+        }
+    };
 
     match args.action {
         Action::Search { mod_name, limit } => {
