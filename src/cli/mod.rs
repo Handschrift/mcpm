f35064431b76d64db4a6ec50acc14b1c35f42275
handschrift <cahit.kilinc@gmx.de>
2023-03-05 10:17:13 +0100

    code reformatting

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,16 +1,8 @@
 use std::borrow::Borrow;
 use std::error::Error;
 use std::fs;
-use std::fs::File;
-use std::io::Write;
-use std::path::{Path, PathBuf};
-
 use clap::Parser;
 use clap::Subcommand;
-use directories::BaseDirs;
-use crate::common::McpmDataError;
-
-use crate::minecraft::MinecraftData;
 
 #[derive(Parser)]
 #[clap(author = "Handschrift", version = "0.0.1", about = "Package manager for minecraft modloaders")]
@@ -23,7 +15,7 @@ pub struct Arguments {
 pub enum Action {
     Search {
         mod_name: String,
-        #[clap(long, short, default_value="20")]
+        #[clap(long, short, default_value = "20")]
         limit: u16,
     },
     Install {
